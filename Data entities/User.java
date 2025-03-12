e356cc3022d0f6881acfd39461b81cb1a2c81ab5
XuanThuNguyen <162556792+XuanThuNguyen@users.noreply.github.com>
2025-03-12 19:03:49 +0700

    modify user data

--- Data entities/User.java
+++ Data entities/User.java
@@ -1,16 +1,21 @@
 import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
-public class Users {
+public class User {
     @Id
     private UUID id;
-    
+
     @Column(unique = true, nullable = false)
     private String email;
-    
+
     @Column(nullable = false)
     private String password;
-    
+
     private String role;
 }
