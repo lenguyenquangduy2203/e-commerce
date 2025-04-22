236aa9334d00ce6af2660fc9bd3eb64b119c6f1a
heineken <lenguyenquanduy@gmail.com>
2025-04-22 08:41:37 +0700

    Move handlers to its own layer

--- dss/app.py
+++ dss/app.py
@@ -1,7 +1,8 @@
-from dash import Dash, dcc, Output, Input
+from dash import Dash, dcc
 import dash_bootstrap_components as dbc
 from dash import html
 from views.sample import graph_placeholder
+from web.token import init_token_handler
 
 # Create the Dash app with Bootstrap
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -13,35 +14,12 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H1("Simple DSS Dashboard"), className="mb-4 mt-4")
     ]),
-
     graph_placeholder("Sine Wave Placeholder"),
     graph_placeholder("Cosine Wave Placeholder"),
     graph_placeholder("Combined Wave Placeholder")
 ], fluid=True)
 
-app.clientside_callback(
-    """
-    function(n_intervals) {
-        if (n_intervals > 0) {
-            return sessionStorage.getItem('token');
-        }
-
-        return window.dash_clientside.no_update;
-    }
-    """,
-    Output('auth-token', 'data'),
-    Input('load-token-once', 'n_intervals')
-)
-
-@app.callback(
-    Output('auth-token', 'data', allow_duplicate=True),
-    Input('auth-token', 'data'),
-    prevent_initial_call='initial_duplicate'
-)
-def show_token(token):
-    print(f"Token received from client: {token}" if token else "No token found.")
-
-    return token
+init_token_handler(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
