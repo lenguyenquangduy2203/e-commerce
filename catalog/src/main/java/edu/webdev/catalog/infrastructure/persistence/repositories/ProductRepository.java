8da000985c684c6dbcc5dae35e5045d47d6021e4
heineken <lenguyenquanduy@gmail.com>
2025-05-01 15:33:58 +0700

    Implement RetrieveProduct query handling with associated query and repository updates

--- catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
+++ catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
@@ -1,7 +1,12 @@
 package edu.webdev.catalog.infrastructure.persistence.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.webdev.catalog.infrastructure.persistence.models.Product;
+import edu.webdev.catalog.infrastructure.persistence.repositories.projections.ProductDetail;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<ProductDetail> findProductDetailById(Long id);
+}
