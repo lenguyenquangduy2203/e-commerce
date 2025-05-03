6c9d908ea0a4e3df09fd93084ae0ed18958b3a40
heineken <lenguyenquanduy@gmail.com>
2025-05-03 14:33:32 +0700

    Add missing @Repository annotation to ProductRepository interface

--- catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
+++ catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import edu.webdev.catalog.infrastructure.persistence.models.Product;
 import edu.webdev.catalog.infrastructure.persistence.repositories.projections.ProductDetail;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<ProductDetail> findProductDetailById(Long id);
     <T> Page<T> findAll(Specification<Product> spec, Pageable pageable, Class<T> projection);
