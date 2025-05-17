001ab315fbf69b67d3e405c58cb6cbdd8e76808f
Siuuuu75085 <147909040+Siuuuu75085@users.noreply.github.com>
2025-05-17 13:25:50 +0700

    Add files via upload

---
+++ orders_data_handler.py
@@ -0,0 +1,9 @@
+# orders_data_handler.py
+import pandas as pd
+
+def load_orders_data(file_path='orders.csv'):
+    df = pd.read_csv(file_path)
+    df.dropna(subset=["customer_id", "order_amount", "order_date"], inplace=True)
+    df["order_date"] = pd.to_datetime(df["order_date"])
+    df["order_amount"] = pd.to_numeric(df["order_amount"])
+    return df
