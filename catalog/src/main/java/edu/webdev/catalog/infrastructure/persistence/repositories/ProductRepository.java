8ee7d7ce4a2158ace69cfa58a55621db8a957c13
heineken <lenguyenquanduy@gmail.com>
2025-05-29 15:00:43 +0700

    Remove unused imports and deprecated method from ProductRepository

--- catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
+++ catalog/src/main/java/edu/webdev/catalog/infrastructure/persistence/repositories/ProductRepository.java
@@ -2,12 +2,8 @@ package edu.webdev.catalog.infrastructure.persistence.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.webdev.catalog.infrastructure.persistence.models.Product;
@@ -16,7 +12,4 @@ import edu.webdev.catalog.infrastructure.persistence.repositories.projections.Pr
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<ProductDetail> findProductDetailById(Long id);
-    
-    @Query("SELECT p FROM Product p WHERE (:spec IS NULL OR :spec = true)")
-    <T> Page<T> findAllProjectedBy(Specification<Product> spec, Pageable pageable, Class<T> projection);
 }
